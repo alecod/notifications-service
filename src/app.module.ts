ea0dd5a4df4c9def90237fd3761be419896a0ec9
Alessandro Spoto <alecodx@gmail.com>
2024-05-29 17:02:05 -0300

    remove kafka instance

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,8 @@ import { Module } from '@nestjs/common';
 
 import { DatabaseModule } from './infra/database/database.module';
 import { HttpModule } from './infra/http/http.module';
-import { MessagingModule } from './infra/messaging/messaging.module';
 
 @Module({
-  imports: [HttpModule, DatabaseModule, MessagingModule],
+  imports: [HttpModule, DatabaseModule],
 })
 export class AppModule {}
